f84b6ec8bb741dca5418ca8002a0890f674fa17e
Bernard Mordan <bernardmordan@gmail.com>
2017-05-15 09:54:22 +0100

    :alien:

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float jumpForce = 16f;
+	public LayerMask groundLayer;
+	private Rigidbody2D rigidBody;
+
+	void Awake () {
+		rigidBody = GetComponent<Rigidbody2D> ();
+	}
+
+	void Update () {
+		if (Input.GetButtonDown ("Jump"))
+			Jump ();
+	}
+
+	void Jump () {
+		if (IsGrounded())
+		    rigidBody.AddForce (Vector2.up * jumpForce, ForceMode2D.Impulse);
+	}
+
+	bool IsGrounded () {
+		if (Physics2D.Raycast(this.transform.position, Vector2.down, 0.2f, groundLayer.value)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
